452980bbd4ce2c0d54283a0f74b8fa9156376dfb
renanabfreire <renanabfreire@gmail.com>
2025-07-25 10:53:47 -0300

    insertion done

--- insertion_sort/src/main.rs
+++ insertion_sort/src/main.rs
@@ -1,17 +1,24 @@
-fn insertion_sort<T: std::cmp::PartialOrd>(arr: &mut [T]) {
+fn insertion_sort<T: std::cmp::PartialOrd + Clone>(arr: &mut [T]) {
     for i in 1..arr.len() {
+        let pivo = arr[i].clone();
 
-        let mut j = i - 1;
+        let mut j = i;
 
-        while (j >= 0) && (arr[j] > pivo) {
-            arr[j + 1] = arr[j];
+        while (j > 0) && (arr[j - 1] > pivo) {
+            arr[j] = arr[j - 1].clone();
             j -= 1;
         }
 
-        arr[j + 1] = pivo;
+        arr[j] = pivo;
     }
 }
 
 fn main() {
-    
+    let mut a = [4, 2, 3, 7, 1, 8, 9, 4, 3, 1];
+
+    insertion_sort(&mut a);
+
+    for i in 0..a.len() {
+        println!("{}", a[i]);
+    }
 }
